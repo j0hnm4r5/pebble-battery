0a8d8436d1434dc88b8e5f066b096603c7a670cd
John Mars <john@m4r5.io>
2015-01-22 11:33:04 -0500

    Apply battery percentage example

--- src/battery.c
+++ src/battery.c
@@ -1,35 +1,53 @@
 #include <pebble.h>
 
 static Window *s_main_window;
+static TextLayer *s_output_layer;
+
+static void battery_handler(BatteryChargeState new_state) {
+  // Write to buffer and display
+  static char s_battery_buffer[32];
+  snprintf(s_battery_buffer, sizeof(s_battery_buffer), "Current battery level: %d/100", new_state.charge_percent);
+  text_layer_set_text(s_output_layer, s_battery_buffer);
+}
 
 static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
 
+  // Create output TextLayer
+  s_output_layer = text_layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
+  text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+
+  // Get the current battery level
+  battery_handler(battery_state_service_peek());
 }
 
 static void main_window_unload(Window *window) {
-
+  // Destroy output TextLayer
+  text_layer_destroy(s_output_layer);
 }
 
-static void init() {
+static void init(void) {
+  // Create main Window
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
 
-	// create the main Window element
-	s_main_window = window_create();
-
-	// set handlers to manage elements inside the Window
-	window_set_window_handlers(s_main_window, (WindowHandlers) {.load = main_window_load, .unload = main_window_unload});
-
-	// show the window on the watch. animated=true
-	window_stack_push(s_main_window, true);
-
+  // Subscribe to the Battery State Service
+  battery_state_service_subscribe(battery_handler);
 }
 
-static void deinit() {
-	// destroy the Window
-	window_destroy(s_main_window);
+static void deinit(void) {
+  // Destroy main Window
+  window_destroy(s_main_window);
 }
 
 int main(void) {
-	init();
-	app_event_loop();
-	deinit();
+  init();
+  app_event_loop();
+  deinit();
 }
